0e223cea7370275439cad74100c015499bd86148
Zach Padovano <zacharyaz10@gmail.com>
2023-05-01 07:08:03 -0700

    LINQ first commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGameTitles = new List<string>() { "Fortnite", "Apex Legends", "Minecraft", "Hogwarts Legacy", "Star Wars: Jedi Fallen Order", "High on Life", "Fallout 4", "Grand Theft Auto 5", "Tiny Tina's Wonderlands" };
+
+            videoGameTitles.Add("League of Legends");
+            videoGameTitles.Add("Assassins Creed Odyssey");
+            videoGameTitles.Add("My Hero One's Justice");
+            videoGameTitles.Add("Naruto Shippuden: Ultimate Ninja Storm 4");
+            videoGameTitles.Add("Minecraft Dungeons");
+            videoGameTitles.Add("South Park: The Stick of Truth");
+            videoGameTitles.Add("The Last of Us");
+            videoGameTitles.Add("Totally Accurate Battle Simulator");
+            videoGameTitles.Add("Dragon Ball Xenoverse 2");
+            videoGameTitles.Add("Dragon Quest XI: Echoes of an Elusive Age");
+
+
+
+           var orderedTitles = videoGameTitles.OrderByDescending(name  => name.Length);
+
+            foreach (var title in orderedTitles) 
+            {
+                Console.WriteLine(title);
+            }
         }
     }
 }
